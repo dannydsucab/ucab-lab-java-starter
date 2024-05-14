51f9d04c0618e165a8dda18808b2e0b6a3ab8e92
Cleteci <cleteci@Cletecis-MacBook-Pro.local>
2024-05-14 19:10:45 -0400

    feat: se agrega librería de Lombok.

--- src/main/java/ve/edu/ucab/lab/Main.java
+++ src/main/java/ve/edu/ucab/lab/Main.java
@@ -1,11 +1,15 @@
 package ve.edu.ucab.lab;
 
+
+import lombok.extern.java.Log;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@Log
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello UCAB!");
+        log.info("Hello UCAB!");
     }
 }
