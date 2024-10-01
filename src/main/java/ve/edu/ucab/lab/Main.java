992a31a2023078299e328df2de3825f9c4936471
dannyds26 <danny.dossantos@gmail.com>
2024-10-01 13:50:23 -0400

    feat: se agrega checkstyle para validación de estilos en código fuente.

--- src/main/java/ve/edu/ucab/lab/Main.java
+++ src/main/java/ve/edu/ucab/lab/Main.java
@@ -1,15 +1,19 @@
 package ve.edu.ucab.lab;
 
-
-import lombok.extern.java.Log;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-@Log
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        log.info("Hello UCAB!");
-    }
+  protected Main() {
+  }
+
+  /**
+   * Método principal de la aplicación que se ejecuta al iniciar el programa.
+   * Es el punto de entrada de cualquier aplicación Java.
+   *
+   * @param args Argumentos de la línea de comandos pasados al programa.
+   *             En este caso, no se utilizan.
+   */
+  public static void main(final String[] args) {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    System.out.println("Hello UCAB!");
+  }
 }
